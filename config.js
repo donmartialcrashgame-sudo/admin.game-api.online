9787e586e4c05666c6cb2dca94097689b52dd91b
game_api <donmartialcrashgame@gmail.com>
2026-09-02 12:12:20 +0100

    Prevent theme client from loading on admin login

--- config.js
+++ config.js
@@ -3,4 +3,8 @@ window.GAME_API_CONFIG={
   SUPABASE_URL:'https://qbagxeqquskkjksoraiz.supabase.co',
   SUPABASE_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJxYmFneGVxcXVza2tqa3NvcmFpejIiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4NzMyMzg5OCwiZXhwIjoyMTAyODk5ODk4fQ.CpvwORFZrDtM5TVzqUlkHyc39E4jofCebQ16Ne-wShk'
 };
-(function(){var s=document.createElement('script');s.src='theme.js';s.defer=true;document.head.appendChild(s)})();
+(function(){
+  var path=(location.pathname||'').split('/').pop().toLowerCase();
+  if(path==='index.html'||path==='')return;
+  var s=document.createElement('script');s.src='theme.js';s.defer=true;document.head.appendChild(s);
+})();
