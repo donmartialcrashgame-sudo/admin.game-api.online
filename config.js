5912ab824f2deee0d8cb11476953a94a37e64cdd
game_api <donmartialcrashgame@gmail.com>
2026-09-02 04:17:39 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Frontend-safe Supabase configuration only. Never place the service-role/secret key here.
 window.GAME_API_CONFIG={
   SUPABASE_URL:'https://qbagxeqquskkjksoraiz.supabase.co',
-  SUPABASE_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJxYmFneGVxcXVza2tqa3NvcmFpejIiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4NzMyMzg5OCwiZXhwIjoyMTAyODk5ODk4fQ.CpvwORFZrDtM5TVzqUlkHyc39E4jofCebQ16Ne-wShk'
+  SUPABASE_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFiYWd4ZXFxdXNra2prc29yYWl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczMjM4OTgsImV4cCI6MjEwMjg5OTg5OH0.CpvwORFZrDtM5TVzqUlkHyc39E4jofCebQ16Ne-wShk'
 };
 (function(){
   var path=(location.pathname||'').split('/').pop().toLowerCase();
